43816872e72816ab07516b6a8cf109ea7404b187
Pr0grammer-learner <tima.privalov@bk.ru>
2024-11-09 13:16:31 +0300

    change test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,10 @@
+from app import app
 import pytest
-from app import app, db
 
-# Настройка приложения для тестирования
 @pytest.fixture
 def client():
-    # Изменяем конфигурацию для использования SQLite в памяти
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    
     with app.test_client() as client:
-        with app.app_context():
-            db.create_all()  # Создаем все таблицы для тестовой БД
         yield client
-        with app.app_context():
-            db.session.remove()
-            db.drop_all()  # Удаляем все таблицы после тестов
-
-# Тест для проверки главной страницы
-def test_home_page(client):
-    response = client.get('/')
-    assert response.status_code == 200  # Ожидаем код 200 для главной страницы
-    assert 'Добро пожаловать в документацию API для mirea-api.ru' in response.data.decode('utf-8')  # Проверяем содержимое docs.html
 
 # Тест для маршрута, возвращающего данные
 def test_data_page(client):
@@ -28,22 +12,12 @@ def test_data_page(client):
     assert response.status_code == 200
     assert b'This is some data!' in response.data
 
-# Тест для создания и получения пользователя
-def test_get_user(client):
-    # Создайте тестового пользователя, чтобы убедиться, что он есть в базе данных
-    response = client.post('/users', json={'username': 'john_doe', 'email': 'john@example.com'})
-    assert response.status_code == 200
-
-    # Проверьте, что маршрут /users/1 возвращает данные этого пользователя
-    response = client.get('/users/1')
-    assert response.status_code == 200
-    assert b'john_doe' in response.data
-
-# Тест для кэша
+# Тест кэширования
 def test_cache(client):
     response1 = client.get('/data')
     response2 = client.get('/data')
-    assert response1.data == response2.data  # Данные должны быть одинаковыми из-за кэша
+    # Данные должны быть одинаковыми из-за кэширования
+    assert response1.data == response2.data
 
 # Тест для маршрута 404
 def test_404(client):
